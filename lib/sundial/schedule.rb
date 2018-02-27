855a21e0a9c6d85911ab859e9620fa1dc7f383d6
Jill Guyonnet <jguyonnet@zendesk.com>
2018-02-27 17:42:06 +0000

    Add 9-17 constraint on #elapsed

--- lib/sundial/schedule.rb
+++ lib/sundial/schedule.rb
@@ -4,7 +4,7 @@ module Sundial
       raise ArgumentError.new("The end time must be later than the start time.") if from > to
 
       if same_day?(from, to)
-        return Sundial::Duration.new(Integer(to - from)) unless weekend?(from)
+        return Sundial::Duration.new(Integer(to_previous_business_hour(to) - to_next_business_hour(from))) unless weekend?(from)
       end
 
       Sundial::Duration.new(0)
@@ -18,6 +18,22 @@ module Sundial
 
     def weekend?(t)
       t.saturday? || t.sunday?
+    end
+
+    def to_next_business_hour(t)
+      if t.hour < 9
+        Time.new(t.year, t.month, t.day, 9)
+      else
+        t
+      end
+    end
+
+    def to_previous_business_hour(t)
+      if t.hour > 17
+        Time.new(t.year, t.month, t.day, 17)
+      else
+        t
+      end
     end
   end
 end
--- spec/schedule_spec.rb
+++ spec/schedule_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Sundial::Schedule do
     end
 
     context 'when both times are on the same day' do
-      context 'when it is on a business day' do
+      context 'and within business hours' do
         it 'returns the elapsed time' do
           from = Time.new(2018, 2, 14, 10, 0, 0)
           to   = Time.new(2018, 2, 14, 11, 2, 5)
@@ -27,6 +27,24 @@ RSpec.describe Sundial::Schedule do
           to   = Time.new(2018, 2, 25, 11, 2, 5)
 
           expect(schedule.elapsed(from, to).in_seconds).to eq 0
+        end
+      end
+
+      context 'when t1 is outside business hours and t2 is inside business hours' do
+        it 'returns the elapsed time from the beginning of the business hours' do
+          from = Time.new(2018, 2, 14, 8)
+          to   = Time.new(2018, 2, 14, 11)
+
+          expect(schedule.elapsed(from, to).in_hours).to eq 2
+        end
+      end
+
+      context 'when t1 is inside business hours and t2 is outside business hours' do
+        it 'returns the elapsed time to the end of the business hours' do
+          from = Time.new(2018, 2, 14, 11)
+          to   = Time.new(2018, 2, 14, 19)
+
+          expect(schedule.elapsed(from, to).in_hours).to eq 6
         end
       end
     end
